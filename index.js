8401bee7ee5a06703bece32f44c8da9c8d211250
Kus Cámara <kcmr@users.noreply.github.com>
2017-08-06 23:12:19 +0200

    fix(index): error if file doesn’t exits in custom project type

--- index.js
+++ index.js
@@ -8,16 +8,35 @@ const os = require('os');
 const argv = yargs.argv;
 let type = argv.type || 'component';
 
+const defaultTypes = ['component', 'app'];
+
 const getFile = (file) => {
   const homedir = os.homedir();
-  return fs.existsSync(`${homedir}/.vscode/vsproject/${type}/${file}.json`)
-    ? `${homedir}/.vscode/vsproject/${type}/${file}.json`
-    : path.resolve(__dirname, `vscode/${type}/${file}.json`);
+  let settingsFile;
+
+  if (fs.existsSync(`${homedir}/.vscode/vsproject/${type}/${file}.json`)) {
+    settingsFile = `${homedir}/.vscode/vsproject/${type}/${file}.json`;
+  } else if (defaultTypes.indexOf(type) > 0) {
+    settingsFile = path.resolve(__dirname, `vscode/${type}/${file}.json`);
+  } else {
+    return console.log(`${file}.json not found in ~/.vscode/${type}/`);
+  }
+
+  return settingsFile;
 };
 
 let settings = getFile('settings');
 let tasks = getFile('tasks');
 
-fs.copySync(settings, '.vscode/settings.json');
-fs.copySync(tasks, '.vscode/tasks.json');
-console.log('🍺  VSCode project set!');
+if (settings) {
+  fs.copySync(settings, '.vscode/settings.json');
+}
+if (tasks) {
+  fs.copySync(tasks, '.vscode/tasks.json');
+}
+
+if (settings || tasks) {
+  console.log('🍺  VSCode project set!');
+} else {
+  console.log('VSCode project not set');
+}
